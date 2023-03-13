fce49a2404b72d59cf7047771f3e71a1862b3bb9
yadhir.maharaj <yadhir.maharaj@thesunexchange.com>
2023-03-13 18:13:11 +0200

    yadz-1 - removed secret EISH! and added functionality to only show 10 breweries

--- brew-track/src/components/BreweryList.tsx
+++ brew-track/src/components/BreweryList.tsx
@@ -1,21 +1,21 @@
-import { Box, Grid, Paper, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Grid, Paper, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
 
 interface BreweryDetails {
-  id: number,
-  breweryName :  string,
-  brewery_type: string,
-  street: string,
-  city: string,
-  state: string,
-  postal_code: string,
-  country: string,
-  longitude: string,
-  latitude: string,
-  phone: string,
-  website_url: string,
-  updated_at: string,
-  tag_list: []
+  id: number;
+  breweryName: string;
+  brewery_type: string;
+  street: string;
+  city: string;
+  state: string;
+  postal_code: string;
+  country: string;
+  longitude: string;
+  latitude: string;
+  phone: string;
+  website_url: string;
+  updated_at: string;
+  tag_list: [];
 }
 
 interface Props {
@@ -23,35 +23,59 @@ interface Props {
 }
 
 const BreweryList: React.FC<Props> = ({ breweries }) => {
+  const [numToShow, setNumToShow] = useState(10);
+
+  const handleShowMore = () => {
+    setNumToShow(numToShow + 10);
+  };
+
+  const visibleBreweries = breweries.slice(0, numToShow);
+
   return (
-    <Grid container spacing={2}>
-      {breweries.map((brewery) => (
-        <Grid key={brewery.id} item xs={12} sm={6} md={4}>
-          <Paper sx={{ p: 2 }}>
-            <Typography variant="h6" component="h3" gutterBottom>
-              {brewery.breweryName}
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom>
-              {brewery.street}
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom>
-              {brewery.city}, {brewery.state} {brewery.postal_code}
-            </Typography>
-            <Box sx={{ mt: 2 }}>
-              <Typography variant="subtitle2" gutterBottom>
-                Type: {brewery.brewery_type}
+    <>
+      <Grid container spacing={2}>
+        {visibleBreweries.map((brewery) => (
+          <Grid key={brewery.id} item xs={12} sm={6} md={4}>
+            <Paper sx={{ p: 2 }}>
+              <Typography variant="h6" component="h3" gutterBottom>
+                {brewery.breweryName}
               </Typography>
-              <Typography variant="subtitle2" gutterBottom>
-                Phone: {brewery.phone}
+              <Typography variant="subtitle1" gutterBottom>
+                {brewery.street}
               </Typography>
-              <Typography variant="subtitle2" gutterBottom>
-                Website: {brewery.website_url}
+              <Typography variant="subtitle1" gutterBottom>
+                {brewery.city}, {brewery.state} {brewery.postal_code}
               </Typography>
-            </Box>
-          </Paper>
-        </Grid>
-      ))}
-    </Grid>
+              <Box sx={{ mt: 2 }}>
+                <Typography variant="subtitle2" gutterBottom>
+                  Type: {brewery.brewery_type}
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Phone: {brewery.phone}
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Website:{" "}
+                  <a
+                    href={brewery.website_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {brewery.website_url}
+                  </a>
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+      {numToShow < breweries.length && (
+        <Box sx={{ mt: 2 }}>
+          <Button variant="contained" onClick={handleShowMore}>
+            Show More
+          </Button>
+        </Box>
+      )}
+    </>
   );
 };
 
